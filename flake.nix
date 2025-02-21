64f0576213331f58953b6c1e66126baf5bd088a3
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:19:53 +0000

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- flake.nix
+++ flake.nix
@@ -1,74 +1,73 @@
 # SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
-
 #
 # SPDX-License-Identifier: GPL-3.0-only
-
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   inputs.nixfigs-helpers.url = "github:shymega/nixfigs-helpers";
 
-  outputs =
-    { self, ... }@inputs:
-    let
-      genPkgs = system: import inputs.nixpkgs { inherit system; };
+  outputs = {self, ...} @ inputs: let
+    genPkgs = system: import inputs.nixpkgs {inherit system;};
 
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
 
-      treeFmtEachSystem =
-        f: inputs.nixpkgs.lib.genAttrs systems (system: f inputs.nixpkgs.legacyPackages.${system});
-      treeFmtEval = treeFmtEachSystem (
-        pkgs:
-        inputs.nixfigs-helpers.inputs.treefmt-nix.lib.evalModule pkgs "${inputs.nixfigs-helpers.helpers.formatter
+    treeFmtEachSystem = f: inputs.nixpkgs.lib.genAttrs systems (system: f inputs.nixpkgs.legacyPackages.${system});
+    treeFmtEval = treeFmtEachSystem (
+      pkgs:
+        inputs.nixfigs-helpers.inputs.treefmt-nix.lib.evalModule pkgs "${
+          inputs.nixfigs-helpers.helpers.formatter
         }"
-      );
+    );
 
-      forEachSystem = inputs.nixpkgs.lib.genAttrs systems;
-    in
-    {
-      # for `nix fmt`
-      formatter = treeFmtEachSystem (pkgs: treeFmtEval.${pkgs.system}.config.build.wrapper);
-      # for `nix flake check`
-      checks =
-        treeFmtEachSystem (pkgs: {
-          formatting = treeFmtEval.${pkgs}.config.build.wrapper;
-        })
-        // forEachSystem (system: {
-          pre-commit-check = import "${inputs.nixfigs-helpers.helpers.checks}" {
-            inherit self system;
-            inherit (inputs.nixfigs-helpers) inputs;
-            inherit (inputs.nixpkgs) lib;
-          };
-        });
-      devShells = forEachSystem (
-        system:
-        let
-          pkgs = genPkgs system;
-        in
-        import inputs.nixfigs-helpers.helpers.devShells { inherit pkgs self system; }
-      );
-      roles = [
-        "RNET"
-        "clockwork_tests"
-        "container"
-        "darwin_arm64"
-        "darwin_x86"
-        "deck"
-        "embedded"
-        "gaming"
-        "gpd_wm2"
-        "minimal"
-        "personal"
-        "proxmox_lxc"
-        "proxmox_vm"
-        "raspberrypi_4"
-        "raspberrypi_z2w"
-        "raspberrypi_zw"
-        "shyNET"
-        "work"
-        "wsl"
-      ];
-    };
+    forEachSystem = inputs.nixpkgs.lib.genAttrs systems;
+  in {
+    # for `nix fmt`
+    formatter = treeFmtEachSystem (pkgs: treeFmtEval.${pkgs.system}.config.build.wrapper);
+    # for `nix flake check`
+    checks =
+      treeFmtEachSystem (pkgs: {
+        formatting = treeFmtEval.${pkgs}.config.build.wrapper;
+      })
+      // forEachSystem (system: {
+        pre-commit-check = import "${inputs.nixfigs-helpers.helpers.checks}" {
+          inherit self system;
+          inherit (inputs.nixfigs-helpers) inputs;
+          inherit (inputs.nixpkgs) lib;
+        };
+      });
+    devShells = forEachSystem (
+      system: let
+        pkgs = genPkgs system;
+      in
+        import inputs.nixfigs-helpers.helpers.devShells {inherit pkgs self system;}
+    );
+    roles = [
+      "rnet"
+      "clockwork_tests"
+      "container"
+      "darwin_arm64"
+      "darwin_x86"
+      "embedded"
+      "gaming"
+      "gh-runner"
+      "gpd_wm2"
+      "minimal"
+      "personal"
+      "proxmox_lxc"
+      "proxmox_vm"
+      "raspberrypi_4"
+      "raspberrypi_z2w"
+      "raspberrypi_zw"
+      "shynet"
+      "steamdeck"
+      "work"
+      "workstation"
+      "wsl"
+    ];
+    checkRole = role:
+      builtins.elem role self.roles;
+    checkRoles = roles: builtins.all (role: self.checkRole role) roles;
+  };
 }
